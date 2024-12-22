59ef3da6a054605f22f60b1aa6bdeaaf2c623862
Micae283 <163884467+Micae283@users.noreply.github.com>
2024-12-22 23:35:14 +0700

    app.py

--- app/app.py
+++ app/app.py
@@ -1,27 +1,43 @@
 
-from flask import Flask, render_template, request, redirect, url_for
+# from flask import Flask, render_template, request, redirect, url_for
+# app = Flask(__name__)
+
+# # Home route
+# @app.route('/')
+# def home():
+#     return render_template('home.html', message="Welcome to my Flask Web App!")
+
+# # About route
+# @app.route('/about')
+# def about():
+#     return render_template('about.html', title="About", description="This is a simple Flask application.")
+
+# # Contact form route
+# @app.route('/contact', methods=['GET', 'POST'])
+# def contact():
+#     if request.method == 'POST':
+#         name = request.form['name']
+#         email = request.form['email']
+#         message = request.form['message']
+#         # Normally, you would save this data or send an email
+#         return render_template('contact.html', success=True, name=name)
+#     return render_template('contact.html', success=False)
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
-# Home route
 @app.route('/')
 def home():
-    return render_template('home.html', message="Welcome to my Flask Web App!")
+    return render_template('home.html')
 
-# About route
-@app.route('/about')
-def about():
-    return render_template('about.html', title="About", description="This is a simple Flask application.")
-
-# Contact form route
-@app.route('/contact', methods=['GET', 'POST'])
-def contact():
-    if request.method == 'POST':
-        name = request.form['name']
-        email = request.form['email']
-        message = request.form['message']
-        # Normally, you would save this data or send an email
-        return render_template('contact.html', success=True, name=name)
-    return render_template('contact.html', success=False)
+@app.route('/next')
+def next_page():
+    return render_template('next.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
